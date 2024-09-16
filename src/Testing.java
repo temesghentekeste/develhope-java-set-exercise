c7077b74e0b7dc351f1098ab98dc3e7232c3d84b
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-09-16 13:18:57 +0300

    Init commit

---
+++ src/Testing.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/*
+Define a testing class with a main() method where you:
+
+create an HashSet with all the days of the week and print the content of the HashSet
+create another implementation of Set that preserves the insertion order of the days of the week and print it
+print the new Set
+print the answer to the question Is the first Set equal to the second one?
+ */
+public class Testing {
+    public static void main(String[] args) {
+
+    }
+}
